d467643dd81fbe0037ee9cf88161e3c6c6b24c0e
fbicciato <f.bicciato.lavoro@gmail.com>
2023-09-05 02:06:45 +0200

    fix string stretch method add pdf generaion with docker compatibility

--- CleaningBracketsAPI.Test/PdfGenearatorTest.cs
+++ CleaningBracketsAPI.Test/PdfGenearatorTest.cs
@@ -26,6 +26,13 @@ namespace CleaningBracketsAPI.Test
 		[InlineData(new string[] { "AAAA", "AAAA", "AAAA", "AAAA", "AAAA", "AAAA", "AAAA", "AAAA" },
 					new string[] { "       AAAA", "      AAAA", "     AAAA", "    AAAA", "   AAAA", "  AAAA", " AAAA", "AAAA" }
 		)]
+		[InlineData(new string[] { "Ciao","bellissimo","marmellata","marmellata","carciofo","panevo2","Ciao","CIo","CI",
+									"STAMPA4", "STAMPA2", "STAMPA2", "STAMPA1","STAMPA","STAMP" },
+					new string[] { "      marmellata", "     marmellata", "    bellissimo", "     carciofo",
+									"     STAMPA1","    STAMPA2","   STAMPA2","  STAMPA4"," panevo2"," STAMPA",
+									" STAMP"," Ciao","CIo","CI"
+									}
+		)]
 		public void ShouldstretchStringIfLenghtIsduplicateString(string[] inputStrings, string[] expectedResults)
 		{
 			
@@ -33,7 +40,7 @@ namespace CleaningBracketsAPI.Test
 			//is allready done in constructor
 			//Act			
 			var list = inputStrings.ToList();
-			_pdfGenerator.ResizeEquelsStingLenght(ref list);
+			_pdfGenerator.ResizeEqualStringLengths(ref list);
 			// Assert
 			Assert.Equal(list, expectedResults);
 		}
--- CleaningBracketsAPI/Logic/Pdf/PdfGenerator.cs
+++ CleaningBracketsAPI/Logic/Pdf/PdfGenerator.cs
@@ -1,30 +1,36 @@
 ﻿
+//using BitMiracle.LibTiff.Classic;
+//using System;
+//using System.Drawing.Imaging;
+//using System.Drawing.Printing;
+//using WkHtmlToPdfDotNet;
+
 namespace CleaningBracketsAPI.Logic.Pdf
 {
-    public class PdfGenerator : IPdfGenerator
-    {
-        private readonly ILogger<PdfGenerator> _logger;
-        private readonly IHttpContextAccessor _context;
+	public class PdfGenerator : IPdfGenerator
+	{
+		private readonly ILogger<PdfGenerator> _logger;
+		private readonly IHttpContextAccessor _context;
 		private readonly IPdfHtmlGenerator _htmlGenerator;
 		private readonly IStringMapsGenerator _stringMapsGenerator;
 
 		public PdfGenerator(ILogger<PdfGenerator> logger, IHttpContextAccessor context, IPdfHtmlGenerator htmlGenerator, IStringMapsGenerator stringMapsGenerator)
-        {
-            _logger = logger;
-            _context = context;
-            _htmlGenerator = htmlGenerator;
+		{
+			_logger = logger;
+			_context = context;
+			_htmlGenerator = htmlGenerator;
 			_stringMapsGenerator = stringMapsGenerator;
 		}
 
-        public byte[] GeneratePdfAndRetriveByte(List<string> inputString)
-        {
+		public byte[] GeneratePdfAndRetriveByte(List<string> inputString)
+		{
 			var endpoint = _context.HttpContext?.Request?.Path;
 			var ipAddress = _context.HttpContext?.Connection?.RemoteIpAddress;
 			_logger.LogInformation($"Request to endpoint {endpoint} from IP {ipAddress}");
 			var PdfStream = new byte[0];
 			try
 			{
-				 ResizeEquelsStingLenght(ref inputString);
+				ResizeEquelsStingLenght(ref inputString);
 				var longhestString = inputString.Max(x => x.Length);
 
 				_stringMapsGenerator.Initialize(longhestString, longhestString);
@@ -34,13 +40,18 @@ namespace CleaningBracketsAPI.Logic.Pdf
 				var pdf = htmlToPdf.ConvertHtmlString(html);
 				PdfStream = pdf.Save();
 				pdf.Close();
+
+
+
+				//var Renderer = new IronPdf.ChromePdfRenderer().RenderHtmlAsPdf(html);
+				//PdfStream = Renderer.BinaryData;
 			}
 			catch (Exception ex)
 			{
 				_logger.LogError(ex, "Error during pdf generation");
 			}
 
-            return PdfStream;
+			return PdfStream;
 		}
 		public async Task<byte[]> GeneratePdfAndRetriveByteAsync(List<string> inputString)
 			=> await Task.Factory.StartNew(() => GeneratePdfAndRetriveByte(inputString));
@@ -48,23 +59,62 @@ namespace CleaningBracketsAPI.Logic.Pdf
 		/// <summary>
 		/// Modify the length of strings by adding a space character to strings with equal lengths.
 		/// </summary>
-		/// <param name="inputStrings"></param>
+		/// <param name="inputStrings"></param>		
 		public void ResizeEquelsStingLenght(ref List<string> inputStrings)
 		{
-			inputStrings = inputStrings.OrderByDescending(x => x.Length).ToList();
-			if (inputStrings.Count() == 1)
-				return;
-
-			for (int i = 1; i < inputStrings.Count(); i++)
+			if (inputStrings == null || inputStrings.Count <= 1)
 			{
-				if (inputStrings[i].Length == inputStrings[i - 1].Length)
+				// Non c'è nulla da fare se la lista è nulla o ha meno di due stringhe
+				return;
+			}
+
+			// Ordina la lista per lunghezza in modo crescente
+			inputStrings = inputStrings.OrderBy(x => x.Length).ToList();
+
+			bool hasEqualLengths = true;
+
+			while (hasEqualLengths)
+			{
+				hasEqualLengths = false;
+
+				for (int i = 0; i < inputStrings.Count - 1; i++)
 				{
-					inputStrings[i] = " " + inputStrings[i];
-					ResizeEquelsStingLenght(ref inputStrings);
+					if (inputStrings[i].Length == inputStrings[i + 1].Length)
+					{
+						inputStrings[i + 1] = " " + inputStrings[i + 1];
+						hasEqualLengths = true;
+					}
 				}
 			}
+
+			// Ordina le stringhe per lunghezza in modo decrescente
+			inputStrings = inputStrings.OrderByDescending(x => x.Length).ToList();
 		}
 
+		public void ResizeEqualStringLengths(ref List<string> inputStrings)
+		{
+			// Ordina la lista in modo crescente per lunghezza
+			inputStrings = inputStrings.OrderBy(x => x.Length).ToList();
+			var check = true;
+			while (check)
+			{
+				check = false;
+				for (int i = 0; i < inputStrings.Count; i++)
+				{
+
+					if ( i< inputStrings.Count-1 && inputStrings[i].Length >= inputStrings[i +1].Length)
+					{
+						// Aggiungi uno spazio all'inizio della stringa successiva
+						inputStrings[i + 1] = " " + inputStrings[i+1];
+						check = true;
+					}
+				}
+			}
+			inputStrings = inputStrings.OrderByDescending(x => x.Length).ToList();
+
+		}
+
+		
 
 	}
 }
